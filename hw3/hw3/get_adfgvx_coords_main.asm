b77954688c92bbafffcfa67d4fb0abd96f8b2000
Neil Opena <neil1114o@gmail.com>
2018-11-09 14:47:02 -0500

    Finished Part I

--- hw3/hw3/get_adfgvx_coords_main.asm
+++ hw3/hw3/get_adfgvx_coords_main.asm
@@ -119,6 +119,19 @@ move $a0, $v1
 syscall
 li $a0, '\n'
 syscall
+# (5, 5)
+li $a0, 5
+li $a1, 5
+jal get_adfgvx_coords
+move $a0, $v0
+li $v0, 11
+syscall
+li $a0, ' '
+syscall
+move $a0, $v1
+syscall
+li $a0, '\n'
+syscall
 
 li $v0, 10
 syscall
