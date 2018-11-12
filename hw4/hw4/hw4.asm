61e10fa5f44d7dfe7ff234375ad6ea51fd0422ba
Neil Opena <neil1114o@gmail.com>
2018-11-12 13:52:13 -0500

    Initialized hw4 files

---
+++ hw4/hw4/hw4.asm
@@ -0,0 +1,94 @@
+# type your first and last name here
+# type your Net ID here (e.g., jmsmith)
+# type your SBU ID # here (e.g., 111234567)
+
+#####################################################################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+##### ANY LINES BEGINNING .data WILL BE DELETED DURING GRADING! #####
+#####################################################################
+
+.text
+
+# Part I
+init_game:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part II
+is_valid_cell:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part III
+get_cell:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part IV
+set_cell:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part V
+reveal_area:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+# Part VI
+get_attack_target:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part VII
+monster_attacks:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part VIII
+player_move:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part IX
+complete_attack:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part X
+player_turn:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+# Part XI
+flood_fill_reveal:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+##### ANY LINES BEGINNING .data WILL BE DELETED DURING GRADING! #####
+#####################################################################
